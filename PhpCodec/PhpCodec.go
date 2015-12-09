ae339b3b11a1b4ca5815a0d1f248c938bc1a81bf
kmiku7 <kakoimiku@gmail.com>
2015-12-09 12:31:57 +0800

    add PhpCodec

---
+++ PhpCodec/PhpCodec.go
@@ -0,0 +1,17 @@
+package PhpCodec
+
+import (
+    "bufio"
+)
+
+func Serialize(object interface{}) data string, err error {
+    
+}
+
+func Unserialize(data string, object interface{}) err error {
+    
+}
+
+func Serialize(object interface{}, w bufio.Writer) err error {
+    
+}
